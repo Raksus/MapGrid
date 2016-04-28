57144ad52af8c6b6dae5755c42df58f0d6f3842c
Raksus <irda22r@gmail.com>
2016-04-28 10:53:54 +0200

    NewGrid  With Click

---
+++ Assets/Scripts/SlabScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class SlabScript : MonoBehaviour {
+    private int pista;
+    private Vector3 posicion;
+
+    public GameObject slabPrefab;
+
+    public Vector3 getPosicion() {
+        return posicion;
+    }
+
+    public SlabScript setPosicion(Vector3 posicion) {
+        this.posicion = posicion;
+        return this;
+    }
+
+    public SlabScript setPosicion(int largo, int ancho) {
+        this.posicion = new Vector3(largo, 0, ancho);
+        return this;
+    }
+
+    public int getPista() {
+        return pista;
+    }
+
+    public SlabScript setPista(int pista) {
+        this.pista = pista;
+        return this;
+    }
+
+    /*void Update() {
+        if (Input.GetMouseButtonDown(0))
+            Debug.Log("Pressed left click.");
+    }*/
+
+    void OnMouseDown() {
+        Debug.Log("Pressed left click.");
+        Renderer rend = GetComponent<Renderer>();
+        rend.material.shader = Shader.Find("Specular");
+        rend.material.SetColor("_SpecColor", Color.red);
+    }
+}
---
+++ Assets/Scripts/TableroManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class TableroManager : MonoBehaviour {
+    public int largo;
+    public int ancho;
+
+    public GameObject slabPrefab;
+
+    private GameObject[][] slabs;
+
+    void Start () {
+        slabs = new GameObject[largo][];
+        for (int l = 0; l < largo; l++) {
+            slabs[l] = new GameObject[ancho];
+            for (int a = 0; a < ancho; a++) {
+                slabs[l][a] = MakeSlab(l, a);
+            }
+        }
+	}
+
+    public GameObject MakeSlab(int largo, int ancho) {
+        Vector3 posicion = new Vector3(largo, 0, ancho);
+
+        GameObject slab = Instantiate(slabPrefab, posicion, Quaternion.identity) as GameObject;
+
+        slab.name = "slab" + largo + ancho;
+        slab.AddComponent<SlabScript>();
+        slab.GetComponent<SlabScript>().setPosicion(posicion);
+        slab.GetComponent<SlabScript>().setPista(ancho);
+
+        return slab;
+    }
+}
